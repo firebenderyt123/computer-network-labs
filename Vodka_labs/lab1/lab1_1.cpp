29fdaf5c862431ccdcb37dd0409a78cc3d06721f
firebenderyt123 <firebenderyt@gmail.com>
2022-11-24 15:10:38 +0200

    lab1 Vodka report

--- Vodka_labs/lab1/lab1_1.cpp
+++ Vodka_labs/lab1/lab1_1.cpp
@@ -9,21 +9,21 @@ int data = 0;
 
 void th_1()
 {
-	while (data != 1000) {
+	if (data != 1000 && data != -1000) {
 		data++;
 		cout<<"Thread 1:"<<data<<endl;
+		return th_1();
 	}
-	terminate();
 	return;
 }
 
 void th_2()
 {
-	while (data != -1000) {
+	if (data != 1000 && data != -1000) {
 		data--;
 		cout<<"Thread 2: "<<data<<endl;
+		return th_2();
 	}
-	terminate();
 	return;
 }
 
